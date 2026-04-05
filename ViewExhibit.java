8f6202b8d165c1f9a17fdaba8062324a1e02a515
ronsxz <johnaaronfulay@gmail.com>
2026-04-05 23:59:20 +0000

    Implementation of View Exhibit Class

---
+++ ViewExhibit.java
@@ -0,0 +1,20 @@
+
+public class Exhibit {
+    private int id;
+        private String title;
+            private String description;
+                private String multimedia; 
+
+                    public Exhibit(int id, String title, String description, String multimedia) {
+                            this.id = id;
+                                    this.title = title;
+                                            this.description = description;
+                                                    this.multimedia = multimedia;
+                                                        }
+
+                                                            // Getters
+                                                                public int getId() { return id; }
+                                                                    public String getTitle() { return title; }
+                                                                        public String getDescription() { return description; }
+                                                                            public String getMultimedia() { return multimedia; }
+                                                                            }
